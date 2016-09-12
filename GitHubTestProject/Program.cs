6a69ee8d9af4a1cdc8d22abb595e0d6310853103
mattaranya <USER1@DOMAIN.local>
2016-09-12 14:42:34 +0300

    toms fixed my code

--- GitHubTestProject/Program.cs
+++ GitHubTestProject/Program.cs
@@ -20,11 +20,11 @@ namespace GitHubTestProject
         /// <returns></returns>
         public static bool IsPrime(int n)
         {
-            bool Is = false;
-            for (int i = 0; i < Math.Sqrt(n); i++)
+            bool Is = true;
+            for (int i = 2; i < Math.Sqrt(n); i++)
             {
                 if (n % i == 0)
-                    Is = true;
+                    Is = false;
             }
             return Is;
         }
